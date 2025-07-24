5b72e1dcdf11d9edde7693a0014404ca6a39839b
tfreese <community@freese-home.de>
2025-07-24 17:52:43 +0200

    polish

--- core/musik-reports.sh
+++ core/musik-reports.sh
@@ -8,7 +8,8 @@ BASEDIR="$(dirname "$0")" # Script directory
 cd "$BASEDIR" || exit
 
 rm -rf ../.gradle/configuration-cache/
-../gradlew --quiet :core:build :core:runMultimediaReporter
+#../gradlew --quiet :core:build :core:runMultimediaReporter
+../gradlew --quiet :core:runMultimediaReporter
 #gradle --quiet :core:build :core:runMultimediaReporter
 
 cd ~ || exit
